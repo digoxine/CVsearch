88ef319944d3f3c951e35bbaacc85b0354d01118
Sacha Memmi <5996895-MemmiS@users.noreply.gitlab.com>
2020-11-23 16:13:18 +0100

    Initialisation du projet

---
+++ src/main/java/com/sorbonne/cvsearches/CvsearchesApplication.java
@@ -0,0 +1,13 @@
+package com.sorbonne.cvsearches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvsearchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvsearchesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sorbonne/cvsearches/CvsearchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorbonne.cvsearches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvsearchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
